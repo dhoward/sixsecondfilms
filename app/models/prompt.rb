9f40843adee31355d44012a1ce8b78f4696bdf11
Daniel Howard <danjhoward@gmail.com>
2013-02-10 17:58:51 -0800

    Do not delete old prompts

--- app/models/prompt.rb
+++ app/models/prompt.rb
@@ -5,11 +5,11 @@ class Prompt < ActiveRecord::Base
   attr_accessible :prompt_category, :prompt_item, :hashtag
 
   def get_tweet_text
-	"#{prompt_category.text} #{prompt_item.text} \##{prompt_item.hashtag}6sf" 
+	 "#{prompt_category.text} #{prompt_item.text} \##{prompt_item.hashtag}6sf" 
   end
 
   def pretty_timestamp
-	created_at.in_time_zone("Pacific Time (US & Canada)").strftime("%l %p %-m/%-d %Z")
+	 created_at.in_time_zone("Pacific Time (US & Canada)").strftime("%l %p %-m/%-d %Z")
   end
 
   def determine_winners  	
@@ -17,23 +17,20 @@ class Prompt < ActiveRecord::Base
   	     .where(:hashtag_used => hashtag)
   	     .find(:all, :order => "retweet_count desc", :limit => 3)
 
-	tweets.each do |tweet|
-		tweet.winner_of = id
-		tweet.save
-	end  	     
+  	tweets.each do |tweet|
+  		tweet.winner_of = id
+  		tweet.save
+  	end  	     
   end
 
   def self.generate_next
-	begin
+
+    begin      
       category = PromptCategory.offset(rand(PromptCategory.count)).first
       random_num = rand(category.prompt_items.count)
       item = PromptItem.where("prompt_category_id = ?", category.id)
                        .offset(random_num).first
-    end while !Prompt.find_by_prompt_category_id_and_prompt_item_id(category.id, item.id).nil?    
-
-    if(Prompt.count > 24)
-      Prompt.first.destroy
-    end
+    end while !Prompt.where('created_at > ?', 1.days.ago).find_by_prompt_category_id_and_prompt_item_id(category.id, item.id).nil?    
 
     next_hashtag = "#{item.hashtag}6sf"
 
