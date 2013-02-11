497721cfcf0a4bf90bb79e87b37025adca2ad7fa
Daniel Howard <danjhoward@gmail.com>
2013-02-10 16:57:01 -0800

    Routed /admin to prompt item index and moved all editing functionality to one page

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,8 @@ Sixsecfilms::Application.routes.draw do
 
   root :to => 'prompts#index'
 
+  match "/admin" => "prompt_items#index"
+
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
