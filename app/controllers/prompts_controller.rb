ad006de3a524458ec299576b7aa673688b335da3
Daniel Howard <danjhoward@gmail.com>
2014-08-18 10:26:04 -0700

    Remove dynamic prompts from demo

--- app/controllers/prompts_controller.rb
+++ app/controllers/prompts_controller.rb
@@ -1,8 +1,8 @@
 class PromptsController < ApplicationController
 
 	def index
-		@prompts = Prompt.find(:all, :order => "id desc", :limit => 25)
-		@current_prompt = @prompts.shift(1).first
+		# @prompts = Prompt.find(:all, :order => "id desc", :limit => 25)
+		# @current_prompt = @prompts.shift(1).first
 	end
 
 end
